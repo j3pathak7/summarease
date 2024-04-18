300e0fd5aff8e02e094b035843eb934676175eab
Jyotirmoy Pathak <jyotirmoypathak.37@gmail.com>
2024-04-18 16:29:23 +0530

    f

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
         <button
           type="button"
           onClick={() =>
-            window.open("https://github.com/TidbitsJS/Summize", "_blank")
+            window.open("https://github.com/j3pathak7/summarease", "_blank")
           }
           className="black_btn"
         >
